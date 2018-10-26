533a7d5f5e2d97946a009472f1712ab1769c0ce5
jaime <jaime@172.26.192.39>
2018-10-26 17:02:54 +0300

    Game

--- src/main/java/ClassTask.java
+++ src/main/java/ClassTask.java
@@ -8,16 +8,38 @@
  *
  * @author jaime
  */
+import java.util.*; 
+import java.io.*;
+
 public class ClassTask {
     
   
     public static void main(String[] args)
     {
-        System.out.println("Test");
+        int i = 0;
+        Scanner inpnumber = new Scanner (System.in); 
         double rnumber =  10*Math.random();
         rnumber = Math.round(rnumber);
-        System.out.println(rnumber);
-        
+        System.out.println("What number is it?. Try to guess it. You have got 3 attempts.");
+        while (i<3)
+        {
+            System.out.println("Give me a integer number between 0 and 10");
+            int number = inpnumber.nextByte();
+            if (rnumber == number)
+            {
+            System.out.println("You win");
+            i = 4;
+            }
+            else
+            {
+                System.out.println("No it is not. You have got " + (2-i) + " attempts.");
+                i++;
+            }
+            if (i == 3)
+            {
+            System.out.println("Game Over.");
+            }
+        }
     }
     
 }
