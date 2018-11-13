20cf53d47742ce12dce12bcb3591e7bf5916ed11
Jaime18001 <Jaime18001@M19C224-D10.LAMK.fi>
2018-11-13 12:10:16 +0200

    With javadoc

---
+++ src/main/java/Annother_class.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jaime18001
+ */
+import java.util.*; 
+
+/**
+ * This class allow you more attempts 
+ * @author Jaime Martínez
+ */
+public class Annother_class {
+    
+    /**
+     * This is the main function of this class
+     * @return i It returns the number of the loop counter.
+     */
+    public static int pa ()
+    {
+        Scanner answer = new Scanner (System.in); 
+        System.out.println("Do you want to play again?/n Set 1  to 1 and 0 to No! ");
+        int answern = answer.nextByte();
+        if (answern == 1)
+        {
+            return 0;
+        }
+        else
+        {
+            return 4;
+        }
+        
+    }
+    
+}
--- src/main/java/ClassTask.java
+++ src/main/java/ClassTask.java
@@ -1,45 +1,90 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+
+
+/**
+ * This is a game in which you have to find a random number in attempts 
+ * @author Jaime Martínez Fernández
+ * @version 1.0
  */
 
 /**
- *
- * @author jaime
+ * I have imported the java.util library
  */
 import java.util.*; 
 
+/**
+ * This is the main class
+ * @author Jaime Martínez
+ */
 public class ClassTask {
     
-  
+  /**
+ * This method is the main method. 
+ * @author Jaime Martínez Fernández
+ * @param args This input is needed
+ */
     public static void main(String[] args)
     {
         int i = 0;
         System.out.println("What number is it?. Try to guess it. You have got 3 attempts.");
+        double rnumber = randomgen();
         while (i<3)
-        {
-            Scanner inpnumber = new Scanner (System.in);
-            double rnumber = randomgen();
-            System.out.println("Give me a integer number between 0 and 10");
-            int number = inpnumber.nextByte();
-            if (rnumber == number)
-            {
-            System.out.println("You win");
-            i = 4;
-            }
-            else
-            {
-                System.out.println("No it is not. You have got " + (2-i) + " attempts.");
-                i++;
-            }
-            if (i == 3)
-            {
-            System.out.println("Game Over.");
-            }
+        {  
+            int number = scannumber();
+            i = compnumber (rnumber, number, i);
         }
         
     } 
+    /**
+     * This method scan an integer number from keyboard
+     * @return number This is the scan number
+     */
+    public static int scannumber()
+    {
+        Scanner inpnumber = new Scanner (System.in); 
+        System.out.println("Give me a integer number between 0 and 10");
+        int number = inpnumber.nextByte();
+        return number;
+    }
+    /**
+     * This method compares the scan number with the random number and print an answer.
+     * @param rnumber This is the random number
+     * @param number This is de keyboard number
+     * @param i This is the loop counter of main function
+     * @return i This method returns the loop counter
+     */
+    public static int compnumber (double rnumber, int number, int i)
+    {
+        if (rnumber == number)
+            {
+            printyw();
+            i = 4;
+            }
+        else
+        {
+            System.out.println("No it is not. You have got " + (2-i) + " attempts.");
+            i++;
+            
+        }
+        if (i == 3)
+        {
+            System.out.println("Game Over.");
+            i = Annother_class.pa();         
+           
+        }
+        return i;
+    }
+    /**
+     * This method only print "You win" when you have found the number in less than three attempts 
+     */
+    public static void printyw()
+    {
+        System.out.println("You win");
+    }
+    
+    /**
+     * This function generates a random integer number between 0 and 10.
+     * @return integer number
+     */
     public static double randomgen()
     { 
         double number =  10*Math.random();
